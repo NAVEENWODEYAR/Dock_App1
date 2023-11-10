dfcce97f76ba464b107a0dd29c1e28996ac27f47
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-10 13:18:58 +0530

    initial commit

---
+++ src/main/java/com/bhas/DockApp1Application.java
@@ -0,0 +1,13 @@
+package com.bhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockApp1Application.class, args);
+	}
+
+}
