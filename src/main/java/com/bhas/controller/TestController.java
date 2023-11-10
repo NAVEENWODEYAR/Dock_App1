fae5bce3c142608b9ffbe3957f8a4dc04a10ca44
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-10 15:03:29 +0530

    log.info("Logging");

---
+++ src/main/java/com/bhas/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.bhas.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+public class TestController 
+{
+	@GetMapping
+	public String greetMsg()
+	{
+		log.info("Logging");
+		return "Welcome to SpringBoot,";
+	}
+}
