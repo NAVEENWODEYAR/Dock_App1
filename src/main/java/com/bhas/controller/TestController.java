ca661dd9c9009d2a4e0bbcd7e74e5afb059cc52f
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-22 22:28:11 +0530

    dockerizing,,

--- src/main/java/com/bhas/controller/TestController.java
+++ src/main/java/com/bhas/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.bhas.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 public class TestController 
 {
 	@GetMapping
+	@ResponseStatus(code = HttpStatus.FOUND)
 	public String greetMsg()
 	{
 		log.info("Logging");
